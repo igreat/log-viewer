ce27e257fa2cd77b7648dc23d307fc6da004a87f
1234567wjw <153204304+1234567wjw@users.noreply.github.com>
2025-03-10 04:31:57 +0000

    Update logService.js

--- client/src/logService.js
+++ client/src/logService.js
@@ -119,6 +119,17 @@ export const renderTable = (logs, id = "filtered-logs", filters = []) => {
     }
 };
 
+function countLogs(theLogs) {
+  const logCount = theLogs.length;
+  document.getElementById('log-count').textContent = logCount;
+  document.getElementById('filtered-log-count').textContent = logCount;
+}
+
+function countFilteredLogs(theFilteredLogs) {
+  const filteredLogs = theFilteredLogs.length;
+  document.getElementById('filtered-log-count').textContent = filteredLogs;
+}
+
 export const applyFilters = (filters) => {
     const filteredLogs = allLogs.filter((log) => {
         if (!isWithinDate(log)) return false;
@@ -139,6 +150,8 @@ export const applyFilters = (filters) => {
 
     renderTable(allLogs, "all-logs", filters);
     renderTable(filteredLogs, "filtered-logs", filters);
+    document.getElementById('log-count').textContent = allLogs.length;
+    document.getElementById('filtered-log-count').textContent = allLogs.length;
 };
 
 export const loadLogFilesModal = () => {
@@ -218,6 +231,8 @@ export const handleFileUploadLocal = (event) => {
             allLogs = getLogsWithIds(data);
             renderTable(allLogs, "all-logs");
             renderTable(allLogs, "filtered-logs");
+            document.getElementById('log-count').textContent = allLogs.length;
+            document.getElementById('filtered-log-count').textContent = allLogs.length;
             console.log("File loaded locally.");
         } catch (error) {
             alert("Error parsing the JSON file. Please upload a valid JSON file.");
@@ -281,9 +296,12 @@ export const handleFileLoad = (id) => {
                 document.getElementById("filtered-logs").innerHTML = `<p class="text-danger">Invalid log format. Expected array but got ${typeof logs}.</p>`;
                 return;
             }
+            countLogs(data);
             allLogs = getLogsWithIds(logs);
             renderTable(allLogs, "all-logs");
             renderTable(allLogs, "filtered-logs");
+            document.getElementById('log-count').textContent = allLogs.length;
+            document.getElementById('filtered-log-count').textContent = allLogs.length;
         })
         .catch(err => {
             console.error("Failed to load logs:", err);
